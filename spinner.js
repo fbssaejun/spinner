0772fae2e851658df389780d984b2dfa97ed05c7
Anthony Kim <fbssaejun@gmail.com>
2021-07-27 19:19:42 +0000

    animation of bar spinning

---
+++ spinner.js
@@ -0,0 +1,20 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 400);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 600);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, 800);
+
+setTimeout(() => {
+  process.stdout.write('\r|   \n'); 
+}, 1000);
+
